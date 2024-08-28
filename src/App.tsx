9ae0ea7c1a7055498dcaf3b512b47f70eae54d79
Vladyslav Popko <vladpopko@gmail.com>
2024-08-28 12:11:54 +0300

    add structure for test task and install libraries

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import './styles/constants.scss'
+import './styles/global.scss'
+
+function App() {
+	return <></>
+}
+
+export default App
---
+++ src/pages/home-page/HomePage.tsx
@@ -0,0 +1,12 @@
+import Home from '../../components/screen/home/Home'
+import './HomePage.module.scss'
+
+const HomePage = () => {
+	return (
+		<>
+			<Home />
+		</>
+	)
+}
+
+export default HomePage
