f2132376e1b4238a24f4d2b87b155d307f3d01b8
Vladyslav Popko <vladpopko@gmail.com>
2024-08-31 20:06:18 +0300

    add styles for message component

--- src/components/screen/chat/chat-content/message/Message.tsx
+++ src/components/screen/chat/chat-content/message/Message.tsx
@@ -1,34 +1,43 @@
+import cn from 'classnames'
 import { useEffect, useState } from 'react'
 import { UseFindUser } from '../../../../../api/user/UseFindUser'
+import { useAppSelector } from '../../../../../store/store'
 import { IMessage } from '../../../../../types/api.types'
 import { IUser } from '../../../../../types/user.types'
 import { formatDate } from '../../../../../utils/formatDate'
 import styles from './Message.module.scss'
 
 const Message = ({ message }: { message: IMessage }) => {
+	const user = useAppSelector(state => state.auth.user)
 	const { mutate } = UseFindUser()
 
-	const [user, setUser] = useState<IUser>()
+	const [author, setAuthor] = useState<IUser>()
+
+	const isMine = user?.id === message.senderId
 
 	useEffect(() => {
 		mutate(
 			{ id: message.senderId },
 			{
 				onSuccess: responseData => {
-					setUser(responseData)
+					setAuthor(responseData)
 				},
 			}
 		)
 	}, [])
 	return (
 		<>
-			<div className={styles.section}>
+			<div
+				className={cn(styles.section, isMine && styles.section_other_message)}
+			>
 				<h2>
-					{user?.name} {user?.surname}
+					{author?.name} {author?.surname}
 				</h2>
-				<h3>{message.text}</h3>
+				<div className={cn(styles.content, isMine && styles.other_message)}>
+					<h3>{message.text}</h3>
+				</div>
+				<p className={styles.date}>{formatDate(message.updatedAt)}</p>{' '}
 			</div>
-			<p className={styles.date}>{formatDate(message.updatedAt)}</p>
 		</>
 	)
 }
