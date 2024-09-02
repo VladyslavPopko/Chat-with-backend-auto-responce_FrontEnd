4be2507008848d4d3b2e5847de475fa9aef057bd
Vladyslav Popko <vladpopko@gmail.com>
2024-09-02 17:31:39 +0300

    add animate menu

--- src/components/layout/menu/MenuLayout.tsx
+++ src/components/layout/menu/MenuLayout.tsx
@@ -1,10 +1,17 @@
+import cn from 'classnames'
+import { useState } from 'react'
 import MenuBody from './menu-body/MenuBody'
 import MenuHeader from './menu-header/MenuHeader'
 import styles from './MenuLayout.module.scss'
 
 const MenuLayout = () => {
+	const [isActive, setIsActive] = useState<boolean>(false)
 	return (
-		<div className={styles.section}>
+		<div
+			className={cn(styles.section, isActive && styles.active)}
+			onMouseEnter={() => setIsActive(true)}
+			onMouseLeave={() => setIsActive(false)}
+		>
 			<MenuHeader />
 			<MenuBody />
 		</div>
